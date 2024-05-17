d9f6f4409be9d096e507850f4fc58512585acf20
mellw0101 <melwivensson@gmail.com>
2024-05-17 17:49:24 +0200

    Changes to be committed: 	deleted:    backup/backup-17-05-24-Fri-15-47-33.tar.gz 	new file:   backup/backup-17-05-24-Fri-17-48-46.tar.gz 	modified:   tools/backup.sh

--- tools/backup.sh
+++ tools/backup.sh
@@ -5,7 +5,9 @@ Date=$(date +"%d-%m-%y-%a-%H-%M-%S")
 mkdir -p backup/tmp
 
 cp -r src backup/tmp/
+cp -r tools backup/tmp/
 cp test backup/tmp/
+cp Makefile backup/tmp/
 
 tar -czf backup/backup.tar.gz --directory=backup/tmp .
 mv backup/backup.tar.gz backup/backup-$Date.tar.gz
