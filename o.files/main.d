1c26954c48bd963139bc14e94c5997d2b4ba9641
mellw0101 <melwivensson@gmail.com>
2024-05-18 11:17:16 +0200

    Changes to be committed: 	modified:   o.files/main.d 	modified:   o.files/main.o 	modified:   test

--- o.files/main.d
+++ o.files/main.d
@@ -1,6 +1,6 @@
 o.files/main.o: src/main.cpp src/data.hpp src/Log.hpp src/globals.h \
   src/tools.hpp src/xcb.hpp src/structs.hpp src/include.hpp \
-  src/defenitions.hpp src/prof.hpp src/color.hpp src/thread.hpp
+  src/defenitions.hpp src/prof.hpp src/color.hpp
 src/data.hpp:
 src/Log.hpp:
 src/globals.h:
@@ -11,4 +11,3 @@ src/include.hpp:
 src/defenitions.hpp:
 src/prof.hpp:
 src/color.hpp:
-src/thread.hpp:
